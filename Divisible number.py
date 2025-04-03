784b4ddc81c7ab8a53c262fad73d1e63d5eff124
AdhritShetty <adhritshetty2009@gmail.com>
2025-04-03 18:48:42 +0000

    Divisible Operator

---
+++ Divisible number.py
@@ -0,0 +1,10 @@
+numerator=int(input("Enter the numerator:"))
+denominator=int(input("Enter the denominator:"))
+if numerator%denominator==0:
+    print("The numerator is divisble by the denominator")
+else:
+    print("The numerator is not divisible by the denominator")
+
+      
+    
+     
