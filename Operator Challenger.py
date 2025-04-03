b75bcb668101e9a95d6391e114d1af851175485c
AdhritShetty <adhritshetty2009@gmail.com>
2025-04-03 18:27:10 +0000

    Operator Challenger

---
+++ Operator Challenger.py
@@ -0,0 +1,16 @@
+v=4
+w=5
+x=3
+y=6
+z=1
+z= (z-y)*x//3
+print("value of (z-y)*x//3 is",z)
+
+
+name="Samar"
+age=17
+
+if name=="Alex" or name == "Raman" and age==3:
+    print("Welcome to my house")
+else:
+    print("You are at the wrong address")
