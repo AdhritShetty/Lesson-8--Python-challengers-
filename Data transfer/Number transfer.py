259eef9ef6c7958622557b424e47a992f3785bd3
AdhritShetty <adhritshetty2009@gmail.com>
2025-04-04 12:44:41 +0000

    Numbers Transfer

---
+++ Data transfer/Number transfer.py
@@ -0,0 +1,15 @@
+a=10
+b=15
+c=20
+if a==10:
+    print("a=15")
+else:
+    print("a=20")
+if b==15:
+    print("b=20")
+else:
+    print("a=15")
+if c==20:
+    print("c=10")
+else:
+    print("b=20")
